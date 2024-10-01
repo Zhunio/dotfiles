2bb51ba0670202556332cc17311027bfba9c1f88
Richard Zhunio <967968+Zhunio@users.noreply.github.com>
2024-10-01 15:46:16 -0400

    Update telescope config

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,15 +1,17 @@
 return {
 	"nvim-telescope/telescope.nvim",
 	tag = "0.1.5",
-	dependencies = { "nvim-lua/plenary.nvim" },
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+	},
 	config = function()
 		local builtin = require("telescope.builtin")
 		vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+		vim.keymap.set("n", "<leader>fp", builtin.commands, {})
+		vim.keymap.set("n", "<leader>ft", builtin.buffers, {})
 		vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-		vim.keymap.set("n", "<leader>f?", ":Telescope current_buffer_fuzzy_find<CR>", {})
-		vim.keymap.set("n", "<leader>fp", ":Telescope commands<CR>", {})
-		vim.keymap.set("n", "<leader>fg", builtin.git_branches, {})
-		vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+		vim.keymap.set("n", "<leader>f?", builtin.current_buffer_fuzzy_find, {})
+		vim.keymap.set("n", "<leader>fb", builtin.git_branches, {})
 		vim.keymap.set("n", "<leader>fr", builtin.registers, {})
 		vim.keymap.set("n", "<leader>fq", builtin.quickfix, {})
 	end,
