8eb4ca30f5c6bccb3e45c9e7748c3a0bc46f321f
Richard Zhunio <967968+Zhunio@users.noreply.github.com>
2024-10-01 00:11:48 -0400

    Refactor telescope plugins

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -6,10 +6,11 @@ return {
 		local builtin = require("telescope.builtin")
 		vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
 		vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-		vim.keymap.set("n", "<leader>fp", ":Telescope current_buffer_fuzzy_find<CR>", {})
-		vim.keymap.set("n", "<leader>fc", ":Telescope commands<CR>", {})
-		vim.keymap.set("n", "<leader>fb", builtin.git_branches, {})
-		vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
-		vim.keymap.set("n", "<leader>fo", builtin.oldfiles, {})
+		vim.keymap.set("n", "<leader>f?", ":Telescope current_buffer_fuzzy_find<CR>", {})
+		vim.keymap.set("n", "<leader>fp", ":Telescope commands<CR>", {})
+		vim.keymap.set("n", "<leader>fg", builtin.git_branches, {})
+		vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+		vim.keymap.set("n", "<leader>fr", builtin.registers, {})
+		vim.keymap.set("n", "<leader>fq", builtin.quickfix, {})
 	end,
 }
