7b9117147c404b5464d604c00651de95f63f4fee
Richard Zhunio <967968+Zhunio@users.noreply.github.com>
2024-10-03 17:08:34 -0400

    update explorer

--- .config/nvim/lua/plugins/explorer.lua
+++ .config/nvim/lua/plugins/explorer.lua
@@ -1,15 +1,24 @@
 return {
-	"nvim-neo-tree/neo-tree.nvim",
-	branch = "v3.x",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-		"MunifTanjim/nui.nvim",
+	{
+		"nvim-tree/nvim-tree.lua",
+		config = function()
+			require("nvim-tree").setup()
+			vim.keymap.set("n", "<leader>ee", ":NvimTreeFindFileToggle<CR>")
+		end,
 	},
-	config = function()
-		require("neo-tree").setup({})
-		vim.keymap.set("n", "<leader>ee", ":Neotree reveal=true position=left toggle=true<CR>")
-		vim.keymap.set("n", "<leader>ef", ":Neotree reveal=true position=float toggle=true<CR>")
-		vim.keymap.set("n", "<leader>ed", ":DBUIToggle<CR>")
-	end,
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v3.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+			"MunifTanjim/nui.nvim",
+		},
+		config = function()
+			require("neo-tree").setup({})
+			-- vim.keymap.set("n", "<leader>ee", ":Neotree reveal=true position=left toggle=true<CR>")
+			vim.keymap.set("n", "<leader>ef", ":Neotree reveal=true position=float toggle=true<CR>")
+			vim.keymap.set("n", "<leader>ed", ":DBUIToggle<CR>")
+		end,
+	},
 }
