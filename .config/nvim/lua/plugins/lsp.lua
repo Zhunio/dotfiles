a205ae02f9755265222e4d29e461dd21c4b42a13
Richard Zhunio <967968+Zhunio@users.noreply.github.com>
2024-10-03 17:09:02 -0400

    java lsp

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,46 +1,49 @@
 return {
+	{
 
-	"williamboman/mason.nvim",
-	dependencies = {
-		"williamboman/mason-lspconfig.nvim",
-		"neovim/nvim-lspconfig",
-	},
-	config = function()
-		require("mason").setup({
-			ui = {
-				icons = {
-					package_installed = "✓",
-					package_pending = "➜",
-					package_uninstalled = "✗",
+		"williamboman/mason.nvim",
+		dependencies = {
+			"williamboman/mason-lspconfig.nvim",
+			"neovim/nvim-lspconfig",
+		},
+		config = function()
+			require("mason").setup({
+				ui = {
+					icons = {
+						package_installed = "✓",
+						package_pending = "➜",
+						package_uninstalled = "✗",
+					},
 				},
-			},
-		})
+			})
 
-		require("mason-lspconfig").setup({
-			ensure_installed = { "lua_ls", "html", "cssls", "ts_ls", "angularls" },
-			automatic_installation = true,
-		})
+			local ensure_installed_servers = { "lua_ls", "html", "cssls", "ts_ls", "angularls", "jdtls" }
+			require("mason-lspconfig").setup({
+				ensure_installed = ensure_installed_servers,
+				automatic_installation = true,
+			})
 
-		local on_attach = function()
-			vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, {})
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-			vim.keymap.set("n", "gr", require("telescope.builtin").lsp_references, {})
-			vim.keymap.set("n", "g=", vim.lsp.buf.code_action, {})
-		end
+			for _, server in ipairs(ensure_installed_servers) do
+				local lsp_config = require("lspconfig")
+				lsp_config[server].setup({
+					on_attach = function()
+						vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, {})
+						vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+						vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+						vim.keymap.set("n", "g=", vim.lsp.buf.code_action, {})
 
-		local lspConfig = require("lspconfig")
-		lspConfig.lua_ls.setup({ on_attach = on_attach })
-		lspConfig.html.setup({ on_attach = on_attach })
-		lspConfig.cssls.setup({ on_attach = on_attach })
-		lspConfig.ts_ls.setup({ on_attach = on_attach })
-		lspConfig.angularls.setup({ on_attach = on_attach })
+						local builtin = require("telescope.builtin")
+						vim.keymap.set("n", "gr", builtin.lsp_references, {})
+					end,
+				})
+			end
 
-		-- Change the Diagnostic symbols in the sign column (gutter)
-		local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
-		for type, icon in pairs(signs) do
-			local hl = "DiagnosticSign" .. type
-			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-		end
-	end,
+			-- Change the Diagnostic symbols in the sign column (gutter)
+			local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
+			for type, icon in pairs(signs) do
+				local hl = "DiagnosticSign" .. type
+				vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+			end
+		end,
+	},
 }
