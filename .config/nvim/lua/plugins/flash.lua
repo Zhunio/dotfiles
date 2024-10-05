1a358e2178c2a28de836296c263d5d70fdd73c95
Richard Zhunio <967968+Zhunio@users.noreply.github.com>
2024-10-04 20:15:28 -0400

    Flash lazy loading

--- .config/nvim/lua/plugins/flash.lua
+++ .config/nvim/lua/plugins/flash.lua
@@ -1,19 +1,13 @@
 return {
 	"folke/flash.nvim",
-	event = "VeryLazy",
+  -- stylua: ignore
+  keys = {
+    { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+    { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+  },
 	opts = {
 		modes = {
 			search = { enabled = true, highlight = { backdrop = true } },
-		},
-	},
-	keys = {
-		{
-			"s",
-			mode = { "n", "x", "o" },
-			function()
-				require("flash").treesitter()
-			end,
-			desc = "Flash Treesitter",
 		},
 	},
 }
