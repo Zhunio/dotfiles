4e1d29d323e85b7857f1463c2ddfc9467620d419
Richard Zhunio <967968+Zhunio@users.noreply.github.com>
2024-10-05 10:55:46 -0400

    update flash

--- .config/nvim/lua/plugins/flash.lua
+++ .config/nvim/lua/plugins/flash.lua
@@ -1,5 +1,6 @@
 return {
 	"folke/flash.nvim",
+	event = { "BufReadPre", "BufNewFile" },
   -- stylua: ignore
   keys = {
     { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
