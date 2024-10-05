de095b49d75e2f26faef7dd2d9cf0b2b6065c314
Richard Zhunio <967968+Zhunio@users.noreply.github.com>
2024-10-04 20:26:21 -0400

    formatting lazy loading

--- .config/nvim/lua/plugins/formatting.lua
+++ .config/nvim/lua/plugins/formatting.lua
@@ -1,6 +1,9 @@
 return {
 	"stevearc/conform.nvim",
-	event = { "BufReadPre", "BufNewFile" },
+  -- stylua: ignore
+	keys = {
+		{ "<leader>f", function() require("conform").format({ lsp_fallback = true, async = false, timeout_ms = 500 }) end },
+	},
 	config = function()
 		require("conform").setup({
 			formatters_by_ft = {
@@ -13,19 +16,6 @@ return {
 				yaml = { "prettier" },
 				markdown = { "prettier" },
 			},
-			format_on_save = {
-				lsp_fallback = true,
-				async = false,
-				timeout_ms = 500,
-			},
 		})
-
-		vim.keymap.set({ "n", "v" }, "<leader>f", function()
-			require("conform").format({
-				lsp_fallback = true,
-				async = false,
-				timeout_ms = 500,
-			})
-		end)
 	end,
 }
