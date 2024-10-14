816606ef97bf182de53a25b5a8a4975a9dbe5d9c
Richard Zhunio <967968+Zhunio@users.noreply.github.com>
2024-10-14 10:47:18 -0400

    Fix nvim colorizer

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -210,7 +210,7 @@ return {
 	},
 	{
 		"norcalli/nvim-colorizer.lua",
-		event = "BufReadPre", "BufNewFile",
+		event = { "BufReadPre", "BufNewFile" },
 		config = function()
 			require("colorizer").setup()
 		end,
