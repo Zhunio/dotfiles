860041fed087a8f4658cc6e35810a4d2dceb6455
Richard Zhunio <967968+Zhunio@users.noreply.github.com>
2024-10-14 00:43:14 -0400

    Improve theme

--- .config/nvim/lua/plugins/editor-theme.lua
+++ .config/nvim/lua/plugins/editor-theme.lua
@@ -4,38 +4,59 @@ return {
 		name = "catppuccin",
 		priority = 1000,
 		config = function()
+			local C = require("catppuccin.palettes").get_palette("mocha")
+			local custom_highlights = {
+        -- Default
+				Constant = { fg = C.teal },
+				Label = { fg = C.mauve },
+				Operator = { fg = C.peach },
+				Exception = { fg = C.mauve },
+				Structure = { fg = C.mauve },
+				Special = { fg = C.mauve },
+        -- lsp
+				["@property"] = { fg = C.text },
+				["@module"] = { fg = C.yellow },
+				["@lsp.type.interface"] = { fg = C.yellow },
+				-- Typescript
+				typescriptTry = { link = "Exception" },
+				typescriptExceptions = { link = "Exception" },
+				typescriptNull = { link = "Keyword" },
+				typescriptVariable = { link = "Keyword" },
+				typescriptPredefinedType = { link = "Keyword" },
+				typescriptOperator = { link = "Keyword" },
+				["@lsp.type.member.typescript"] = { link = "Function" },
+				-- HTML
+				htmlTag = { fg = C.blue },
+				htmlTagN = { link = "htmlTag" },
+				htmlTagName = { link = "htmlTag" },
+				-- SASS
+				sassMixing = { link = "Keyword" },
+				sassMixinName = { link = "Function" },
+				-- CSS
+				cssFontAttr = { fg = C.text },
+				cssTextAttr = { fg = C.text },
+				cssPositioningAttr = { fg = C.text },
+				cssFlexibleBoxAttr = { fg = C.text },
+				-- Java
+				["@lsp.type.annotation.java"] = { fg = C.peach },
+				["@lsp.type.property.java"] = { fg = C.peach },
+				-- SQL
+				sqlType = { link = "Keyword" },
+				-- YAML
+				yamlBlockMappingKey = { fg = C.blue },
+				yamlPlainScalar = { fg = C.green },
+			}
+
 			require("catppuccin").setup({
 				transparent_background = true,
 				integrations = {
 					diffview = true,
 				},
-				highlight_overrides = {
-					mocha = function(mocha)
-						return {
-              LineNr = { fg = mocha.overlay1 },
-							sqlKeyword = { link = "Statement" },
-							yamlBlockMappingKey = { fg = mocha.lavender },
-							yamlPlainScalar = { fg = mocha.green },
-							typescriptImport = { link = "Keyword" },
-							typescriptVariable = { link = "Keyword" },
-							typescriptExport = { link = "Keyword" },
-							htmlTag = { link = "Function" },
-							htmlTagN = { link = "Function" },
-							htmlTagName = { link = "Function" },
-							javaCommentTitle = { link = "Comment" },
-							javaExternal = { fg = mocha.mauve, style = { "italic" } },
-							["@lsp.type.class.java"] = { fg = mocha.yellow, style = { "italic" } },
-							["@lsp.type.namespace.java"] = { fg = mocha.yellow },
-							["@lsp.type.annotation.java"] = { fg = mocha.peach },
-							["@lsp.type.method.java"] = { fg = mocha.blue, style = { "italic" } },
-							["@lsp.mod.static.java"] = { fg = mocha.peach },
-							["@lsp.type.property.java"] = { fg = mocha.text },
-							["@lsp.type.enumMember.java"] = { fg = mocha.teal },
-						}
-					end,
-				},
+				custom_highlights = custom_highlights,
 			})
+
 			vim.cmd("colorscheme catppuccin-mocha")
+			vim.cmd("set cmdheight=1")
 		end,
 	},
 }
