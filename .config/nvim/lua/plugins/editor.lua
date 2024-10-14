75fb3e9173021692d8dcbd359cc39825dd222b5d
Richard Zhunio <967968+Zhunio@users.noreply.github.com>
2024-10-14 10:39:20 -0400

    Update config

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -155,7 +155,6 @@ return {
 		event = { "BufReadPre", "BufNewFile" },
   -- stylua: ignore
   keys = {
-    { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
     { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
   },
 		opts = {
