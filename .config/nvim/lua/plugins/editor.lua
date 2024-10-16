c6e6479e4a1476e6fd7e715be5c3bdf01af1b67f
Richard Zhunio <967968+Zhunio@users.noreply.github.com>
2024-10-16 13:47:21 -0400

    Add buffer close

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -31,6 +31,7 @@ return {
 		"romgrk/barbar.nvim",
 		event = { "BufReadPre", "BufNewFile" },
 		keys = {
+			{ "<leader>q", ":BufferClose<CR>" },
 			{ "<leader>h", ":BufferPrevious<CR>" },
 			{ "<leader>l", ":BufferNext<CR>" },
 			{ "<leader><left>", ":BufferMovePrevious<CR>" },
