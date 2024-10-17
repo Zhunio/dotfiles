ee1e308521d393b8873c3ce8ae52f2e3bb1b8b14
Richard Zhunio <967968+Zhunio@users.noreply.github.com>
2024-10-16 22:53:14 -0400

    Remove noice

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -103,28 +103,6 @@ return {
 		end,
 	},
 	{
-		"folke/noice.nvim",
-		event = "VeryLazy",
-		opts = {
-			presets = {
-				bottom_search = true, -- use a classic bottom cmdline for search
-				long_message_to_split = true, -- long messages will be sent to a split
-				inc_rename = false, -- enables an input dialog for inc-rename.nvim
-				lsp_doc_border = false, -- add a border to hover docs and signature help
-			},
-			cmdline = {
-				format = {
-					cmdline = { icon = "" },
-					search_down = { icon = "" },
-					search_up = { icon = "" },
-					filter = { icon = "$" },
-					lua = { icon = "" },
-					help = { icon = "?" },
-				},
-			},
-		},
-	},
-	{
 		"folke/flash.nvim",
     -- stylua: ignore
     keys = {
