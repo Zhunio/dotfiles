e8cd196c725eda5ca884a6cdc7f8a00b0742348e
Richard Zhunio <967968+Zhunio@users.noreply.github.com>
2024-10-17 00:38:24 -0400

    Comment java development

--- .config/nvim/lua/plugins/language.lua
+++ .config/nvim/lua/plugins/language.lua
@@ -1,14 +1,14 @@
 return {
-	{
-		"mfussenegger/nvim-jdtls",
-		ft = "java",
-		config = function()
-			require("jdtls").start_or_attach({
-				cmd = { vim.fn.stdpath("data") .. "/mason/bin/jdtls" },
-				root_dir = vim.fs.dirname(vim.fs.find({ "gradlew", ".git", "mvnw" }, { upward = true })[1]),
-			})
-		end,
-	},
+	-- {
+	-- 	"mfussenegger/nvim-jdtls",
+	-- 	ft = "java",
+	-- 	config = function()
+	-- 		require("jdtls").start_or_attach({
+	-- 			cmd = { vim.fn.stdpath("data") .. "/mason/bin/jdtls" },
+	-- 			root_dir = vim.fs.dirname(vim.fs.find({ "gradlew", ".git", "mvnw" }, { upward = true })[1]),
+	-- 		})
+	-- 	end,
+	-- },
 	{
 		"MeanderingProgrammer/render-markdown.nvim",
 		ft = { "markdown" },
