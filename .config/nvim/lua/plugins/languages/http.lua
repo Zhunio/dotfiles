3ea773c0bccd35ee3f5f1705dff5551ee8adbe9e
Richard Zhunio <967968+Zhunio@users.noreply.github.com>
2025-01-23 16:06:55 -0500

    feat(curl): add curl

--- .config/nvim/lua/plugins/languages/http.lua
+++ .config/nvim/lua/plugins/languages/http.lua
@@ -1,11 +1,20 @@
 return {
 	{
-		"mistweaverco/kulala.nvim",
-    -- stylua: ignore
-    keys = {
-      { "<leader>kk", function () require('kulala').run() end },
-      { "<leader>kc", function () require('kulala').from_curl() end },
-    },
-		opts = {},
+		"oysandvik94/curl.nvim",
+		cmd = { "CurlOpen" },
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+		config = true,
+		init = function()
+			vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+				pattern = "*.curl",
+				callback = function()
+					vim.bo.filetype = "bash"
+          -- stylua: ignore
+					vim.keymap.set("n", "<CR>", function() require("curl").execute_curl() end, { buffer = 0 })
+				end,
+			})
+		end,
 	},
 }
