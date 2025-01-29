3789f8c83c64cfa74c119d91098e35056014321c
Richard Zhunio <967968+Zhunio@users.noreply.github.com>
2025-01-29 18:17:21 -0500

    feat(jdtls): add java support

--- .config/nvim/lua/plugins/languages/java.lua
+++ .config/nvim/lua/plugins/languages/java.lua
@@ -1,12 +1,12 @@
 return {
-	-- {
-	-- 	"mfussenegger/nvim-jdtls",
-	-- 	ft = "java",
-	-- 	config = function()
-	-- 		require("jdtls").start_or_attach({
-	-- 			cmd = { vim.fn.stdpath("data") .. "/mason/bin/jdtls" },
-	-- 			root_dir = vim.fs.dirname(vim.fs.find({ "gradlew", ".git", "mvnw" }, { upward = true })[1]),
-	-- 		})
-	-- 	end,
-	-- },
+	{
+		"mfussenegger/nvim-jdtls",
+		ft = "java",
+		config = function()
+			require("jdtls").start_or_attach({
+				cmd = { vim.fn.stdpath("data") .. "mason/packages/jdtls/bin/jdtls" },
+				root_dir = vim.fs.dirname(vim.fs.find({ "gradlew", ".git", "mvnw" }, { upward = true })[1]),
+			})
+		end,
+	},
 }
