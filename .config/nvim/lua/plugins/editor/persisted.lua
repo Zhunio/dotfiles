a26030d52520406bb8183d4172102ec64cde359b
Richard Zhunio <967968+Zhunio@users.noreply.github.com>
2025-01-29 18:19:09 -0500

    fix(persisted): fix persisted when opening octo

--- .config/nvim/lua/plugins/editor/persisted.lua
+++ .config/nvim/lua/plugins/editor/persisted.lua
@@ -21,6 +21,18 @@ return {
 				return true
 			end
 
+			vim.api.nvim_create_autocmd("User", {
+				pattern = "PersistedSavePre",
+				callback = function()
+					for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+						for _, filetype in ipairs({ "octo" }) do
+							if vim.bo[buf].filetype == filetype then
+								vim.api.nvim_buf_delete(buf, { force = true })
+							end
+						end
+					end
+				end,
+			})
 			persisted.setup({
 				autoload = true,
 				should_save = should_save,
