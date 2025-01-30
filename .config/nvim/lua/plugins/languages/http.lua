e3a3c9abf305edd64b4beb3d7d294af0f3ebd191
Richard Zhunio <967968+Zhunio@users.noreply.github.com>
2025-01-30 11:49:32 -0500

    fix(kulala): fix kulala lazy loading

--- .config/nvim/lua/plugins/languages/http.lua
+++ .config/nvim/lua/plugins/languages/http.lua
@@ -1,24 +1,7 @@
 return {
 	{
-		"oysandvik94/curl.nvim",
-		cmd = { "CurlOpen" },
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-		},
-		config = true,
-		init = function()
-			vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-				pattern = "*.curl",
-				callback = function()
-					vim.bo.filetype = "bash"
-          -- stylua: ignore
-					vim.keymap.set("n", "<CR>", function() require("curl").execute_curl() end, { buffer = 0 })
-				end,
-			})
-		end,
-	},
-	{
 		"mistweaverco/kulala.nvim",
+		ft = { "http" },
 		opts = {},
 		init = function()
 			vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
