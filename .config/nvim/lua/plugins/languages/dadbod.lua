f8892ba1e350314bc7e06898611fe5042385a340
Richard Zhunio <967968+Zhunio@users.noreply.github.com>
2025-02-16 14:50:13 -0500

    feat(dadbod): improve dadbod config by adding query save location

--- .config/nvim/lua/plugins/languages/dadbod.lua
+++ .config/nvim/lua/plugins/languages/dadbod.lua
@@ -7,26 +7,21 @@ return {
 		"DBUIFindBuffer",
 	},
 	dependencies = {
-		{ "tpope/vim-dadbod" },
-		{ "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" } },
-		{ "ellisonleao/dotenv.nvim", ft = { "sql", "mysql", "plsql" } },
-		{ "hrsh7th/nvim-cmp" },
+		{ "tpope/vim-dadbod", lazy = true },
+		{ "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true },
+		{ "hrsh7th/nvim-cmp", lazy = true },
 	},
 	config = function() end,
 	init = function()
 		vim.g.db_ui_use_nerd_fonts = 1
 		vim.g.db_ui_winwidth = 50
+		vim.g.db_ui_save_location = vim.fn.expand("~/.dadbod")
 
 		vim.api.nvim_create_autocmd("FileType", {
 			pattern = { "sql", "mysql", "plsql" },
 			callback = function()
 				require("cmp").setup.buffer({ sources = { { name = "vim-dadbod-completion" } } })
 			end,
-		})
-
-		require("dotenv").setup({
-			enable_on_load = true, -- will load your .env file upon loading a buffer
-			verbose = false, -- show error notification if .env file is not found and if .env is loaded
 		})
 	end,
 }
