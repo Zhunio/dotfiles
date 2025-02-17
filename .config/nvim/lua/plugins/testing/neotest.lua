d1d6be4928965e5ff99e4882cc0f7ead88fc3b17
Richard Zhunio <967968+Zhunio@users.noreply.github.com>
2025-02-17 17:40:09 -0500

    feat(neotest): add neotest

---
+++ .config/nvim/lua/plugins/testing/neotest.lua
@@ -0,0 +1,41 @@
+return {
+	{
+		"nvim-neotest/neotest",
+    --stylua: ignore
+		keys = {
+			{ "<leader>tt", function() require("neotest").run.run() end },
+			{ "<leader>tl", function() require("neotest").run.run_last() end },
+			{ "<leader>tf", function() require("neotest").run.run(vim.fn.expand("%")) end },
+			{ "<leader>ta", function() require("neotest").run.run({suite = true}) end },
+			{ "<leader>ts", function() require("neotest").run.stop() end },
+			{ "<leader>te", function() require("neotest").summary.toggle() end },
+			{ "<leader>to", function() require("neotest").output.open({ enter = true }) end },
+			{ "<leader>tp", function() require("neotest").output_panel.toggle() end },
+		},
+		dependencies = {
+			"nvim-neotest/nvim-nio",
+			"nvim-lua/plenary.nvim",
+			"antoinemadec/FixCursorHold.nvim",
+			"nvim-treesitter/nvim-treesitter",
+			"nvim-neotest/neotest-jest",
+			"Zhunio/neotest-karma",
+		},
+		config = function()
+			require("neotest").setup({
+				summary = {
+					open = "botright vsplit | vertical resize 100",
+				},
+				adapters = {
+					require("neotest-jest")({
+						jestCommand = "npm run test --",
+						env = { CI = true },
+						cwd = function()
+							return vim.fn.getcwd()
+						end,
+					}),
+					require("neotest-karma")({}),
+				},
+			})
+		end,
+	},
+}
