06bb68b52a33cb54ce98512757e13b7c5a4bccea
Richard Zhunio <967968+Zhunio@users.noreply.github.com>
2025-02-17 17:45:21 -0500

    feat(copilot): move copilot to its own file

---
+++ .config/nvim/lua/plugins/editor/copilot.lua
@@ -0,0 +1,78 @@
+return {
+	{
+		"github/copilot.vim",
+		ft = { "python", "lua", "typescript" },
+		cond = function()
+			return vim.fn.executable("gh") == 1
+		end,
+		init = function()
+			vim.keymap.set("i", "<M-w>", "<Plug>(copilot-accept-word)")
+			vim.keymap.set("i", "<M-e>", "<Plug>(copilot-accept-line)")
+		end,
+	},
+	{
+		"CopilotC-Nvim/CopilotChat.nvim",
+		cmd = { "CopilotChat" },
+		cond = function()
+			return vim.fn.executable("gh") == 1
+		end,
+		keys = {
+			{
+				"<leader>gg",
+				function()
+					local current_win = vim.api.nvim_get_current_win()
+					local win_width = vim.api.nvim_win_get_width(current_win)
+
+					if win_width < 160 then
+						require("CopilotChat").open({ window = { layout = "horizontal" } })
+					else
+						require("CopilotChat").open({ window = { layout = "vertical" } })
+					end
+				end,
+				mode = { "n", "v" },
+			},
+			{ "<leader>gx", ":CopilotChatClose<CR>", mode = { "n", "v" } },
+			{ "<leader>gs", ":CopilotChatStop<CR>", mode = { "n", "v" } },
+			{ "<leader>gr", ":CopilotChatReset<CR>", mode = { "n", "v" } },
+			{
+				"<leader>gp",
+				function()
+					local prompts = {
+						"Explain",
+						"Review",
+						"Fix",
+						"Optimize",
+						"Docs",
+						"Tests",
+						"Commit",
+					}
+
+					vim.ui.select(prompts, { prompt = "Choose a prompt:" }, function(prompt)
+						if prompt then
+							vim.cmd("CopilotChat" .. prompt .. "<CR>")
+						end
+					end)
+				end,
+				mode = { "n", "v" },
+			},
+		},
+		dependencies = {
+			{ "github/copilot.vim" },
+			{ "nvim-lua/plenary.nvim", branch = "master" },
+		},
+		build = "make tiktoken", -- Only on MacOS or Linux
+		opts = {
+			highlight_headers = false,
+			error_header = "> [!ERROR] Error",
+			mappings = {
+				complete = {
+					insert = "<C-p>",
+				},
+				reset = {
+					normal = "<C-r>",
+					insert = "<C-r>",
+				},
+			},
+		},
+	},
+}
