62a89db50d0c9d8cc745f33adb2faa83a452c71c
Richard Zhunio <967968+Zhunio@users.noreply.github.com>
2025-03-05 12:38:54 -0500

    feat(persistence.nvim): add session manager to neovim

---
+++ .config/nvim/lua/plugins/editor/persistence.lua
@@ -0,0 +1,16 @@
+return {
+	"folke/persistence.nvim",
+  --stylua: ignore
+  keys = {
+    { "<leader>pp", function() require('persistence').load() end },
+    { "<leader>ps", function() require('persistence').stop() end },
+  },
+	config = function()
+		require("persistence").setup({
+			options = { "globals" },
+			pre_save = function()
+				vim.api.nvim_exec_autocmds("User", { pattern = "SessionSavePre" })
+			end,
+		})
+	end,
+}
