09c81a470d235b496ade6d39368da12ebe4704a3
Richard Zhunio <967968+Zhunio@users.noreply.github.com>
2025-03-06 11:30:58 -0500

    feat(persisted.nvim): add ignored filetypes

--- .config/nvim/lua/plugins/editor/persisted.lua
+++ .config/nvim/lua/plugins/editor/persisted.lua
@@ -8,10 +8,16 @@ return {
 			vim.api.nvim_create_autocmd("User", {
 				pattern = "PersistedSavePre",
 				callback = function()
+					local ignored_filetypes = {
+						"octo",
+						"floggraph",
+						"NeogitStatus",
+					}
+
 					for _, buf in ipairs(vim.api.nvim_list_bufs()) do
 						if vim.bo[buf].filetype == "alpha" then
 							vim.cmd("Alpha")
-						elseif vim.bo[buf].filetype == "octo" then
+						elseif vim.tbl_contains(ignored_filetypes, vim.bo[buf].filetype) then
 							vim.api.nvim_buf_delete(buf, { force = true })
 						end
 					end
