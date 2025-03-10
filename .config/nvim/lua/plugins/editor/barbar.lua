61711ce49ef06d7dc0220c1ea38c852be5ef4753
Richard Zhunio <967968+Zhunio@users.noreply.github.com>
2025-03-10 18:53:05 -0400

    feat(barbar.nvim): move barbar.nvim to its own file

---
+++ .config/nvim/lua/plugins/editor/barbar.lua
@@ -0,0 +1,14 @@
+return {
+	{
+		"romgrk/barbar.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		keys = {
+			{ "<leader>q", ":BufferClose<CR>" },
+			{ "<leader>h", ":BufferPrevious<CR>" },
+			{ "<leader>l", ":BufferNext<CR>" },
+			{ "<leader><left>", ":BufferMovePrevious<CR>" },
+			{ "<leader><right>", ":BufferMoveNext<CR>" },
+		},
+		opts = {},
+	},
+}
--- .config/nvim/lua/plugins/editor/editor.lua
+++ .config/nvim/lua/plugins/editor/editor.lua
@@ -1,18 +1,6 @@
 return {
 	{ "christoomey/vim-tmux-navigator" },
 	{
-		"romgrk/barbar.nvim",
-		event = { "BufReadPre", "BufNewFile" },
-		keys = {
-			{ "<leader>q", ":BufferClose<CR>" },
-			{ "<leader>h", ":BufferPrevious<CR>" },
-			{ "<leader>l", ":BufferNext<CR>" },
-			{ "<leader><left>", ":BufferMovePrevious<CR>" },
-			{ "<leader><right>", ":BufferMoveNext<CR>" },
-		},
-		opts = {},
-	},
-	{
 		"nvim-lualine/lualine.nvim",
 		opts = {
 			options = {
