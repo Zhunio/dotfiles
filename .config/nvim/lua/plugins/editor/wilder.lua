6e67ff7c95068456ef489ae1522252e0bfca8f8f
Richard Zhunio <967968+Zhunio@users.noreply.github.com>
2025-03-11 11:50:32 -0400

    feat(wilder.nvim): add wilder.nvim

---
+++ .config/nvim/lua/plugins/editor/wilder.lua
@@ -0,0 +1,24 @@
+return {
+	{
+		"gelguy/wilder.nvim",
+		config = function()
+			local wilder = require("wilder")
+
+			require("wilder").setup({
+				modes = { ":" },
+			})
+
+			wilder.set_option(
+				"renderer",
+				wilder.popupmenu_renderer(wilder.popupmenu_border_theme({
+					highlights = {
+						border = "Normal", -- highlight to use for the border
+					},
+					-- 'single', 'double', 'rounded' or 'solid'
+					-- can also be a list of 8 characters, see :h wilder#popupmenu_border_theme() for more details
+					border = "rounded",
+				}))
+			)
+		end,
+	},
+}
