4454b4491a285fa67a1b57e612f7ea0a98408b52
Richard Zhunio <967968+Zhunio@users.noreply.github.com>
2025-03-13 16:48:11 -0400

    feat(codesnap.nvim): add codesnap.nvim

--- .config/nvim/lua/plugins/editor/codesnap.lua
+++ .config/nvim/lua/plugins/editor/codesnap.lua
@@ -2,5 +2,14 @@ return {
 	"mistricky/codesnap.nvim",
 	lazy = true,
 	build = "make",
-	cmd = { "CodeSnap", "CodeSnapSave" },
+	cmd = { "CodeSnap", "CodeSnapSave", "CodeSnapHighlight", "CodeSnapSaveHighlight" },
+	opts = {
+		save_path = "~/CodeSnap",
+		has_breadcrumbs = true,
+		has_line_number = true,
+		bg_theme = "default",
+		bg_x_padding = 50,
+		bg_y_padding = 50,
+		bg_padding = nil,
+	},
 }
