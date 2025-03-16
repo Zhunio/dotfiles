b1397c27cb476c5fd46f75de7ec6feb6b3f68d42
Richard Zhunio <967968+Zhunio@users.noreply.github.com>
2025-03-16 16:20:57 -0400

    feat(image.nvim): move image.nvim to its own file

--- .config/nvim/lua/plugins/editor/neo-tree.lua
+++ .config/nvim/lua/plugins/editor/neo-tree.lua
@@ -7,7 +7,7 @@ return {
 			{ "<leader>ef", ":Neotree reveal=true position=float toggle=true<CR>" },
 			{ "<leader>ee", ":Neotree reveal=true position=left toggle=true<CR>" },
 		},
-		dependencies = { { "3rd/image.nvim", opts = {}, lazy = true } },
+		dependencies = { { "3rd/image.nvim", lazy = true } },
 		opts = {
 			filesystem = {
 				filtered_items = {
