d69b94ea3f3cb8de3d46766efa2ad2a8a3b4481f
Richard Zhunio <967968+Zhunio@users.noreply.github.com>
2025-03-17 18:09:50 -0400

    feat(flash.nvim): make it lazy

--- .config/nvim/lua/plugins/editor/flash.lua
+++ .config/nvim/lua/plugins/editor/flash.lua
@@ -1,6 +1,6 @@
 return {
 	"folke/flash.nvim",
-	event = "VeryLazy",
+	lazy = true,
   -- stylua: ignore
   keys = {
     -- { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
