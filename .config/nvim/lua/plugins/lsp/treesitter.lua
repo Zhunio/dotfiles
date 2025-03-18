63fcd4f2e31f88929a47ad3b3a4994b5bd6be421
Richard Zhunio <967968+Zhunio@users.noreply.github.com>
2025-03-18 12:30:44 -0400

    feat(treesitter): disable hightlight for json to fix highlighting in json files

--- .config/nvim/lua/plugins/lsp/treesitter.lua
+++ .config/nvim/lua/plugins/lsp/treesitter.lua
@@ -9,6 +9,7 @@ return {
 				auto_install = true,
 				highlight = {
 					enable = true,
+					disable = { "json" },
 					additional_vim_regex_highlighting = false,
 				},
 				indent = { enable = true },
