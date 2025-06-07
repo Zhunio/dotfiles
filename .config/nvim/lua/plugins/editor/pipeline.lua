bf71d23340d1ef2b7cbb7c389f9029447bf092d5
Richard Zhunio <967968+Zhunio@users.noreply.github.com>
2025-06-07 18:17:44 -0400

    disable pipeline

--- .config/nvim/lua/plugins/editor/pipeline.lua
+++ .config/nvim/lua/plugins/editor/pipeline.lua
@@ -1,14 +1,14 @@
 return {
-	{
-		"topaxi/pipeline.nvim",
-		cmd = { "Pipeline" },
-		build = "make",
-		opts = {
-			split = {
-				relative = "editor",
-				position = "right",
-				size = 100,
-			},
-		},
-	},
+	-- {
+	-- 	"topaxi/pipeline.nvim",
+	-- 	cmd = { "Pipeline" },
+	-- 	build = "make",
+	-- 	opts = {
+	-- 		split = {
+	-- 			relative = "editor",
+	-- 			position = "right",
+	-- 			size = 100,
+	-- 		},
+	-- 	},
+	-- },
 }
