b50e37133cb0e62a7fc75600258f4190703a3744
Richard Zhunio <967968+Zhunio@users.noreply.github.com>
2025-06-14 23:18:25 -0400

    dotfiles: add the latest Homebrew Git bin directory to PATH if it exists

--- dotfiles.sh
+++ dotfiles.sh
@@ -12,3 +12,10 @@ source $HOME/.sdkman/bin/sdkman-init.sh
 eval "$(zoxide init zsh)"
 # oh-my-posh theme
 eval "$(oh-my-posh init zsh --config $HOME/dotfiles/themes/oh-my-posh-theme.json)"
+
+# Add the latest Homebrew Git bin directory to PATH if it exists
+git_dir=$(echo /usr/local/Cellar/git/*/bin | awk '{print $1}')
+if [[ -d "$git_dir" ]]; then
+  export PATH="$git_dir:$PATH"
+fi
+
