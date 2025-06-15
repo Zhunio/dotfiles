83df8c5b27662e8bd68c1cea768bddc785e2871b
Richard Zhunio <967968+Zhunio@users.noreply.github.com>
2025-06-15 08:34:18 -0400

    leetcode.lua: enable leetcode

--- .config/nvim/lua/plugins/editor/leetcode.lua
+++ .config/nvim/lua/plugins/editor/leetcode.lua
@@ -1,12 +1,12 @@
 return {
-	-- {
-	-- 	"kawre/leetcode.nvim",
-	-- 	cmd = { "Leet" },
-	-- 	build = ":TSUpdate html",
-	-- 	dependencies = {
-	-- 		"nvim-lua/plenary.nvim",
-	-- 		"MunifTanjim/nui.nvim",
-	-- 	},
-	-- 	opts = {},
-	-- },
+	{
+		"kawre/leetcode.nvim",
+		cmd = { "Leet" },
+		build = ":TSUpdate html",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"MunifTanjim/nui.nvim",
+		},
+		opts = {},
+	},
 }
