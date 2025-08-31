cb63683f1f5d87ac7f96b2568f00297f49b0ac2b
Richard Zhunio <967968+Zhunio@users.noreply.github.com>
2025-08-31 19:36:38 -0400

    refactor: split mason.lua -> mason-{lspconfig,tool-installer}.lua

---
+++ .config/nvim/lua/plugins/lsp/mason-tool-installer.lua
@@ -0,0 +1,56 @@
+return {
+	{
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		opts = {
+			ensure_installed = {
+				-- bash
+				"bashls",
+				"shfmt",
+
+				-- lua
+				"lua_ls",
+				"stylua",
+
+				-- html, css, js
+				"html",
+				"cssls",
+				"ts_ls",
+				-- sass
+				"some-sass-language-server",
+				"js-debug-adapter",
+
+				-- markdown
+				"marksman",
+
+				-- prettier
+				"prettier",
+
+				-- angular
+				"angularls",
+
+				-- java
+				-- "java-test",
+				-- "java-debug-adapter",
+				-- "lombok-nightly",
+				-- "jdtls",
+				-- "google-java-format",
+
+				-- python
+				"pyright",
+				"black",
+				"isort",
+
+				-- docker
+				"docker-compose-language-service",
+				"dockerfile-language-server",
+
+				-- terraform
+				"terraform-ls",
+
+				-- SQL
+				"sqlls",
+				"sqlfmt",
+			},
+		},
+	},
+}
