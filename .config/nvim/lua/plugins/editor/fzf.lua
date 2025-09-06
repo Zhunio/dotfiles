c93bf194a3ee9c1b937201f8737a09e788713953
Richard Zhunio <967968+Zhunio@users.noreply.github.com>
2025-09-06 15:19:50 -0400

    add description to keymaps

--- .config/nvim/lua/plugins/editor/fzf.lua
+++ .config/nvim/lua/plugins/editor/fzf.lua
@@ -4,33 +4,32 @@ return {
 		cmd = { "FzfLua" },
     -- stylua: ignore
 		keys = {
-			-- Buffers & Files
-			{ "<leader>ff", function() require('fzf-lua').files() end },
-			{ "<leader>fh", function() require("fzf-lua").helptags() end },
-			{ "<leader>fq", function() require("fzf-lua").quickfix() end },
+			-- File operations
+			{ "<leader>ff", function() require('fzf-lua').files() end, desc = "Fzf: Search and open files" },
+			{ "<leader>fh", function() require("fzf-lua").helptags() end, desc = "Fzf: Search Neovim help tags" },
+			{ "<leader>fq", function() require("fzf-lua").quickfix() end, desc = "Fzf: Show quickfix list" },
 
 			-- Search
-			{ "<leader>fg", function() require('fzf-lua').live_grep() end },
-			{ "<leader>f?", function() require('fzf-lua').lgrep_curbuf() end },
+			{ "<leader>fg", function() require('fzf-lua').live_grep() end, desc = "Fzf: Search text in project (live grep)" },
+			{ "<leader>f?", function() require('fzf-lua').lgrep_curbuf() end, desc = "Fzf: Search text in current buffer" },
 
-      -- Git
-			{ "<leader>fb", function() require("fzf-lua").git_branches() end },
+      -- Git integration
+			{ "<leader>fb", function() require("fzf-lua").git_branches() end, desc = "Fzf: List and checkout git branches" },
 
-      -- LSP
-			{ "gr", function() require("fzf-lua").lsp_references() end },
-			{ "gd", function() require("fzf-lua").lsp_definitions() end },
-			{ "gD", function() vim.diagnostic.enable(not vim.diagnostic.is_enabled()) end },
-			{ "g=", function() require('fzf-lua').lsp_code_actions() end },
-			{ "<leader>f@", function() require("fzf-lua").lsp_document_symbols() end },
-			{ "<leader>f#", function() require("fzf-lua").lsp_live_workspace_symbols() end },
-			{ "<leader>fd", function() require("fzf-lua").diagnostics_document() end, },
+      -- LSP (Language Server Protocol)
+			{ "gr", function() require("fzf-lua").lsp_references() end, desc = "Fzf: Find references to symbol under cursor" },
+			{ "gd", function() require("fzf-lua").lsp_definitions() end, desc = "Fzf: Go to definition of symbol under cursor" },
+			{ "gD", function() vim.diagnostic.enable(not vim.diagnostic.is_enabled()) end, desc = "Toggle diagnostics display" },
+			{ "g=", function() require('fzf-lua').lsp_code_actions() end, desc = "Fzf: Show available code actions" },
+			{ "<leader>f@", function() require("fzf-lua").lsp_document_symbols() end, desc = "Fzf: List symbols in current document" },
+			{ "<leader>f#", function() require("fzf-lua").lsp_live_workspace_symbols() end, desc = "Fzf: Search symbols in workspace" },
+			{ "<leader>fd", function() require("fzf-lua").diagnostics_document() end, desc = "Fzf: Show diagnostics for current document" },
 
-      -- Misc
-			{ "<leader>fp", function() require("fzf-lua").commands() end, },
-			{ "<leader>f:", function() require("fzf-lua").command_history() end, },
-			{ "<leader>fk", function() require("fzf-lua").keymaps() end, },
-			{ "<leader>fr", function() require("fzf-lua").registers() end, },
-
+      -- Miscellaneous
+			{ "<leader>fp", function() require("fzf-lua").commands() end, desc = "Fzf: List available commands" },
+			{ "<leader>f:", function() require("fzf-lua").command_history() end, desc = "Fzf: Show command history" },
+			{ "<leader>fk", function() require("fzf-lua").keymaps() end, desc = "Fzf: List keymaps" },
+			{ "<leader>fr", function() require("fzf-lua").registers() end, desc = "Fzf: Show registers" },
 		},
 		opts = {
 			lsp = {
