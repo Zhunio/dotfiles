30519bac91b2596dd7e54e234e736f137efa632c
Richard Zhunio <967968+Zhunio@users.noreply.github.com>
2025-09-06 15:16:08 -0400

    fzf: simplify preview

--- .config/nvim/lua/plugins/editor/fzf.lua
+++ .config/nvim/lua/plugins/editor/fzf.lua
@@ -1,8 +1,3 @@
-local Preview = {
-	hidden = { hidden = true },
-	short = { vertical = "up:40%" },
-}
-
 return {
 	{
 		"ibhagwan/fzf-lua",
@@ -11,43 +6,33 @@ return {
 		keys = {
 			-- Buffers & Files
 			{ "<leader>ff", function() require('fzf-lua').files() end },
-			{ "<leader>fh", function() require("fzf-lua").highlights({ winopts = { preview = Preview.short } }) end },
-			{ "<leader>fq", function() require("fzf-lua").quickfix({ winopts = { preview = Preview.short } }) end },
+			{ "<leader>fh", function() require("fzf-lua").helptags() end },
+			{ "<leader>fq", function() require("fzf-lua").quickfix() end },
 
 			-- Search
-			{ "<leader>fg", function() require('fzf-lua').live_grep({ winopts = { preview = Preview.short } }) end },
-			{ "<leader>f?", function() require('fzf-lua').lgrep_curbuf({ winopts = { preview = Preview.short } }) end },
+			{ "<leader>fg", function() require('fzf-lua').live_grep() end },
+			{ "<leader>f?", function() require('fzf-lua').lgrep_curbuf() end },
 
       -- Git
-			{ "<leader>fb", function() require("fzf-lua").git_branches({ winopts = { preview = Preview.hidden } }) end },
+			{ "<leader>fb", function() require("fzf-lua").git_branches() end },
 
       -- LSP
 			{ "gr", function() require("fzf-lua").lsp_references() end },
 			{ "gd", function() require("fzf-lua").lsp_definitions() end },
 			{ "gD", function() vim.diagnostic.enable(not vim.diagnostic.is_enabled()) end },
 			{ "g=", function() require('fzf-lua').lsp_code_actions() end },
-			{ "<leader>f@", function() require("fzf-lua").lsp_document_symbols({ winopts = { preview = Preview.short } }) end },
-			{ "<leader>f#", function() require("fzf-lua").lsp_live_workspace_symbols({ winopts = { preview = Preview.short } }) end },
-			{ "<leader>fd", function() require("fzf-lua").diagnostics_document({ winopts = { preview = Preview.short } }) end, },
+			{ "<leader>f@", function() require("fzf-lua").lsp_document_symbols() end },
+			{ "<leader>f#", function() require("fzf-lua").lsp_live_workspace_symbols() end },
+			{ "<leader>fd", function() require("fzf-lua").diagnostics_document() end, },
 
       -- Misc
-			{ "<leader>fp", function() require("fzf-lua").commands({ winopts = { preview = Preview.hidden } }) end, },
-			{ "<leader>f:", function() require("fzf-lua").command_history({ winopts = { preview = Preview.hidden } }) end, },
-			{ "<leader>fk", function() require("fzf-lua").keymaps({ winopts = { preview = Preview.hidden } }) end, },
-			{ "<leader>fr", function() require("fzf-lua").registers({ winopts = { preview = Preview.hidden } }) end, },
+			{ "<leader>fp", function() require("fzf-lua").commands() end, },
+			{ "<leader>f:", function() require("fzf-lua").command_history() end, },
+			{ "<leader>fk", function() require("fzf-lua").keymaps() end, },
+			{ "<leader>fr", function() require("fzf-lua").registers() end, },
 
 		},
 		opts = {
-			winopts = {
-				fullscreen = true,
-
-				border = "none",
-				preview = {
-					border = "none",
-					layout = "vertical",
-					vertical = "up:80%",
-				},
-			},
 			lsp = {
 				symbols = {
 					symbol_style = 2,
