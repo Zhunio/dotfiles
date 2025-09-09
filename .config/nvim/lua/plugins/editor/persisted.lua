6a21772380b62c1acd6f95a1f569fa8057a4564c
Richard Zhunio <967968+Zhunio@users.noreply.github.com>
2025-09-09 19:59:35 -0400

    persisted: fix alpha integration in persisted

--- .config/nvim/lua/plugins/editor/persisted.lua
+++ .config/nvim/lua/plugins/editor/persisted.lua
@@ -6,10 +6,9 @@ return {
 			local persisted = require("persisted")
 
 			vim.api.nvim_create_autocmd("User", {
-				pattern = "PersistedSavePre",
+				pattern = "PersistedLoadPost",
 				callback = function()
 					local ignored_filetypes = {
-						"alpha",
 						"octo",
 						"floggraph",
 						"NeogitStatus",
@@ -17,13 +16,25 @@ return {
 						"copilot-chat",
 					}
 
+					local valid_buffers = {}
+
 					for _, buf in ipairs(vim.api.nvim_list_bufs()) do
-						if vim.tbl_contains(ignored_filetypes, vim.bo[buf].filetype) then
+						local is_empty_filetype = vim.bo[buf].filetype == ""
+						local is_ignored_filetype = vim.tbl_contains(ignored_filetypes, vim.bo[buf].filetype)
+
+						if is_empty_filetype or is_ignored_filetype then
 							vim.api.nvim_buf_delete(buf, { force = true })
+						else
+							table.insert(valid_buffers, buf)
 						end
+					end
+
+					if #valid_buffers == 0 then
+						vim.cmd(":Alpha")
 					end
 				end,
 			})
+
 			persisted.setup({
 				autoload = true,
 			})
