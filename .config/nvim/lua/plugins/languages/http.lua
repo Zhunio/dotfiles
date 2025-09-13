d28375f61390e35a8120e78eb0c7c6b01bf87ff8
Richard Zhunio <967968+Zhunio@users.noreply.github.com>
2025-09-13 16:26:03 -0400

    kulala: add key mappings

--- .config/nvim/lua/plugins/languages/http.lua
+++ .config/nvim/lua/plugins/languages/http.lua
@@ -3,6 +3,18 @@ return {
 		"mistweaverco/kulala.nvim",
 		ft = { "http" },
 		opts = {},
+    -- stylua: ignore
+    keys = {
+      { "<leader>ks", function() require("kulala").scratchpad() end, desc = "Kulala: Open scratchpad" },
+      { "<CR>", function() require("kulala").run() end, desc = "Kulala: Run current request" },
+      { "<leader>kc", function() require("kulala").copy() end, desc = "Kulala: Copy current request as curl" },
+      { "<leader>kf", function() require("kulala").from_curl() end, desc = "Kulala: Import from curl" },
+      { "<leader>kq", function() require("kulala").close() end, desc = "Kulala: Close current request" },
+      { "<leader>kt", function() require("kulala").toggle_view() end, desc = "Kulala: Toggle request/response view" },
+      { "<leader>ki", function() require("kulala").inspect() end, desc = "Kulala: Inspect response" },
+      { "gj", function() require("kulala").jump_next() end, desc = "Kulala: Jump to next request" },
+      { "gk", function() require("kulala").jump_prev() end, desc= "Kulala: Jump to previous request" },
+    },
 		init = function()
 			vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
 				pattern = "*.http",
@@ -10,15 +22,6 @@ return {
 				callback = function()
 					vim.bo.filetype = "http"
           vim.cmd("TSEnable highlight")
-
-					vim.keymap.set("n", "<CR>", function() require("kulala").run() end, { buffer = 0 })
-					vim.keymap.set("n", "<leader>kc", function() require("kulala").copy() end, { buffer = 0 })
-					vim.keymap.set("n", "<leader>kf", function() require("kulala").from_curl() end, { buffer = 0 })
-					vim.keymap.set("n", "<leader>kq", function() require("kulala").close() end, { buffer = 0 })
-					vim.keymap.set("n", "<leader>kt", function() require("kulala").toggle_view() end, { buffer = 0 })
-					vim.keymap.set("n", "<leader>ki", function() require("kulala").inspect() end, { buffer = 0 })
-					vim.keymap.set("n", "gj", function() require("kulala").jump_next() end, { buffer = 0 })
-					vim.keymap.set("n", "gk", function() require("kulala").jump_prev() end, { buffer = 0 })
 				end,
 			})
 		end,
