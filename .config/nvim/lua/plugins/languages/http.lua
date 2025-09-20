735cabd3784e26744124f37b797c84f63442fad8
Richard Zhunio <967968+Zhunio@users.noreply.github.com>
2025-09-20 11:38:30 -0400

    kulala: disable those mappings so they don't run on every filetype

--- .config/nvim/lua/plugins/languages/http.lua
+++ .config/nvim/lua/plugins/languages/http.lua
@@ -6,14 +6,14 @@ return {
     -- stylua: ignore
     keys = {
       { "<leader>ks", function() require("kulala").scratchpad() end, desc = "Kulala: Open scratchpad" },
-      { "<CR>", function() require("kulala").run() end, desc = "Kulala: Run current request" },
+      -- { "<CR>", function() require("kulala").run() end, desc = "Kulala: Run current request" },
       { "<leader>kc", function() require("kulala").copy() end, desc = "Kulala: Copy current request as curl" },
       { "<leader>kf", function() require("kulala").from_curl() end, desc = "Kulala: Import from curl" },
       { "<leader>kq", function() require("kulala").close() end, desc = "Kulala: Close current request" },
       { "<leader>kt", function() require("kulala").toggle_view() end, desc = "Kulala: Toggle request/response view" },
       { "<leader>ki", function() require("kulala").inspect() end, desc = "Kulala: Inspect response" },
-      { "gj", function() require("kulala").jump_next() end, desc = "Kulala: Jump to next request" },
-      { "gk", function() require("kulala").jump_prev() end, desc= "Kulala: Jump to previous request" },
+      -- { "gj", function() require("kulala").jump_next() end, desc = "Kulala: Jump to next request" },
+      -- { "gk", function() require("kulala").jump_prev() end, desc= "Kulala: Jump to previous request" },
     },
 		init = function()
 			vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
