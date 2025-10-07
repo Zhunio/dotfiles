01dddcc3366a3270f18105b4fb060f36b8ed625a
Richard Zhunio <967968+Zhunio@users.noreply.github.com>
2025-10-06 22:08:42 -0400

    lsp: improve angularls

--- .config/nvim/lua/config/lsp/servers/angularls.lua
+++ .config/nvim/lua/config/lsp/servers/angularls.lua
@@ -1,20 +1,5 @@
 local on_attach = require("config.lsp.on_attach")
 
-local angularls_path = vim.fn.stdpath("data") .. "/mason/packages/angular-language-server"
-local cmd = {
-	"ngserver",
-	"--stdio",
-	"--tsProbeLocations",
-	angularls_path,
-	"--ngProbeLocations",
-	angularls_path .. "/node_modules/@angular/language-server",
-}
-
 return {
-	cmd = cmd,
 	on_attach = on_attach,
-	on_new_config = function(new_config, new_root_dir)
-		new_config.cmd = cmd
-	end,
-	root_dir = require("lspconfig.util").root_pattern("angular.json"),
 }
