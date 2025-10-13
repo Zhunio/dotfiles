a99748daae2162b331fbd500790c838a27c9c42a
Richard Zhunio <967968+Zhunio@users.noreply.github.com>
2025-10-13 08:52:59 -0400

    bin: add $HOME/bin to path

--- dotfiles.sh
+++ dotfiles.sh
@@ -14,6 +14,11 @@ eval "$(zoxide init zsh)"
 # oh-my-posh theme
 eval "$(oh-my-posh init zsh --config $HOME/dotfiles/themes/oh-my-posh-theme.json)"
 
+# add bin to path
+if [[ -s $HOME/bin ]]; then
+  export PATH="$PATH:$HOME/bin"
+fi
+
 # Add the latest Homebrew Git bin directory to PATH if it exists
 git_dir=$(echo /usr/local/Cellar/git/*/bin | awk '{print $1}')
 if [[ -d "$git_dir" ]]; then
