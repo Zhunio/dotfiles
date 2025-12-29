7b2fea2b1ad5c1782d4b774a93f16e3ea350ee50
Richard Zhunio <967968+Zhunio@users.noreply.github.com>
2025-12-29 14:16:26 -0500

    lsp: move diagnostics to lsp-config

--- .config/nvim/lua/plugins/lsp/mason-lspconfig.lua
+++ .config/nvim/lua/plugins/lsp/mason-lspconfig.lua
@@ -15,13 +15,24 @@ return {
 				"lua_ls",
 				"pylsp",
 				"terraformls",
-        "ts_ls",
+				"ts_ls",
 			}
 
 			for _, server in ipairs(servers) do
 				local server_config = require("config.lsp.servers." .. server)
 				vim.lsp.config(server, server_config)
 			end
+
+			-- Change the Diagnostic symbols in the sign column (gutter)
+			local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+			for type, icon in pairs(signs) do
+				local hl = "DiagnosticSign" .. type
+				vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+			end
+
+			vim.diagnostic.config({
+				virtual_text = true,
+			})
 
 			-- TODO: Update this so that it does not get called by mason-lspconfig, instead it gets called by jdtls
 			-- ["jdtls"] = function() end,
