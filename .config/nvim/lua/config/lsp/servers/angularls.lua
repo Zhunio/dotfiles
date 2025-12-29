ce834f90ca1f9703c4d470e37eb589356830367a
Richard Zhunio <967968+Zhunio@users.noreply.github.com>
2025-12-29 14:25:05 -0500

    angularls: add root_dir

--- .config/nvim/lua/config/lsp/servers/angularls.lua
+++ .config/nvim/lua/config/lsp/servers/angularls.lua
@@ -2,4 +2,11 @@ local on_attach = require("config.lsp.on_attach")
 
 return {
 	on_attach = on_attach,
+	root_dir = function(fname)
+		local lspconfig = require("lspconfig")
+		local root = lspconfig.util.root_pattern("angular.json", "workspace.json", "project.json")(fname)
+
+		-- returning nil prevents the server from starting
+		return root or nil
+	end,
 }
