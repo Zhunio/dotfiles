2be98095c52a6ca552664af4f0724fa53ceca341
Richard Zhunio <967968+Zhunio@users.noreply.github.com>
2025-12-30 16:33:24 -0500

    lsp: add keybindings description

--- .config/nvim/lua/config/lsp/on_attach.lua
+++ .config/nvim/lua/config/lsp/on_attach.lua
@@ -1,4 +1,4 @@
 return function()
-	vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, {})
-	vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+	vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, { desc = "LSP: Rename symbol" })
+	vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "LSP: Hover documentation" })
 end
