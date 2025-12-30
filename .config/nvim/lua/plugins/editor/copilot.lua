70a46205a168ad691e8095feffc6ae89a70f41bd
Richard Zhunio <967968+Zhunio@users.noreply.github.com>
2025-12-30 16:30:49 -0500

    copilot: add keybindings

--- .config/nvim/lua/plugins/editor/copilot.lua
+++ .config/nvim/lua/plugins/editor/copilot.lua
@@ -19,11 +19,11 @@ return {
 		end,
     --stylua: ignore
 		keys = {
-			{ "<leader>gg", function() require("CopilotChat").open({ window = { layout = "vertical" } }) end, mode = { "n", "v" } },
-			{ "<leader>gv", function() require("CopilotChat").open({ window = { layout = "horizontal" } }) end, mode = { "n", "v" } },
-			{ "<leader>gx", ":CopilotChatClose<CR>", mode = { "n", "v" } },
-			{ "<leader>gs", ":CopilotChatStop<CR>", mode = { "n", "v" } },
-			{ "<leader>gr", ":CopilotChatReset<CR>", mode = { "n", "v" } },
+			{ "<leader>gg", function() require("CopilotChat").open({ window = { layout = "vertical" } }) end, mode = { "n", "v" }, desc = "Copilot: Open (Vertical)" },
+			{ "<leader>gs", function() require("CopilotChat").open({ window = { layout = "horizontal" } }) end, mode = { "n", "v" }, desc = "Copilot: Open (Horizontal)" },
+			{ "<leader>gr", ":CopilotChatReset<CR>", mode = { "n", "v" }, desc = "Copilot: Reset" },
+			{ "<leader>gx", ":CopilotChatClose<CR>", mode = { "n", "v" }, desc = "Copilot: Close" },
+			{ "<leader>gq", ":CopilotChatStop<CR>", mode = { "n", "v" }, desc = "Copilot: Stop" },
 			{
 				"<leader>gp",
 				function()
