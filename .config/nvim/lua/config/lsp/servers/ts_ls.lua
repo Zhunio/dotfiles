79fe089ebf6ae60977b9d4096db871e27147434c
Richard Zhunio <967968+Zhunio@users.noreply.github.com>
2026-01-01 18:40:52 -0500

    ts_ls: add organize imports keybinding

--- .config/nvim/lua/config/lsp/servers/ts_ls.lua
+++ .config/nvim/lua/config/lsp/servers/ts_ls.lua
@@ -2,6 +2,15 @@ local on_attach = require("config.lsp.on_attach")
 
 return {
 	cmd = { "typescript-language-server", "--stdio" },
-	on_attach = on_attach,
+	on_attach = function(client, bufnr)
+		on_attach()
+
+		vim.keymap.set("n", "gO", function()
+			vim.lsp.buf.execute_command({
+				command = "_typescript.organizeImports",
+				arguments = { vim.api.nvim_buf_get_name(bufnr) },
+			})
+		end, { desc = "LSP: Organize Imports" })
+	end,
 	filetypes = { "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx" },
 }
