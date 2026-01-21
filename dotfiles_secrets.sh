353985eb4701be573f7e4a0d915b5d9440cdf52f
Richard Zhunio <967968+Zhunio@users.noreply.github.com>
2026-01-20 19:46:13 -0500

    mail: offlineimap, op, msmtp, aerc

--- dotfiles_secrets.sh
+++ dotfiles_secrets.sh
@@ -3,7 +3,7 @@ init_op_secrets() {
   export MAIL_PERSONAL_PASS="$(op read 'op://Development/mail.personal/password')"
 
   export MAIL_WORK_USER="$(op read 'op://Development/mail.work/username')"
-  export MAIL_WORK_OAUTH_TOKEN="$(oauth2l fetch --credentials ~/.oauth/mail.work.json --scope https://mail.google.com)"
+  export MAIL_WORK_OAUTH_TOKEN="$(oauth2l fetch --credentials ~/.oauth/mail.work.json --scope https://mail.google.com --cache ~/.oauth2l --refresh)"
 }
 
 mail() {
