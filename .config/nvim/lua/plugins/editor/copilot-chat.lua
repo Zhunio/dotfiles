51a8fc9e15cd95b268b5e55d86770f5deede4c83
Richard Zhunio <967968+Zhunio@users.noreply.github.com>
2026-02-01 12:51:05 -0500

    copilot: separate copilot + copilot-chat

---
+++ .config/nvim/lua/plugins/editor/copilot-chat.lua
@@ -0,0 +1,55 @@
+return {
+	"CopilotC-Nvim/CopilotChat.nvim",
+	-- cmd = { "CopilotChat" },
+	cond = function()
+		return vim.fn.executable("gh") == 1
+	end,
+    --stylua: ignore
+	keys = {
+		{ "<leader>gg", function() require("CopilotChat").open({ window = { layout = "vertical" } }) end, mode = { "n", "v" }, desc = "Copilot: Open (Vertical)" },
+		{ "<leader>gs", function() require("CopilotChat").open({ window = { layout = "horizontal" } }) end, mode = { "n", "v" }, desc = "Copilot: Open (Horizontal)" },
+		{ "<leader>gr", ":CopilotChatReset<CR>", mode = { "n", "v" }, desc = "Copilot: Reset" },
+		{ "<leader>gx", ":CopilotChatClose<CR>", mode = { "n", "v" }, desc = "Copilot: Close" },
+		{ "<leader>gq", ":CopilotChatStop<CR>", mode = { "n", "v" }, desc = "Copilot: Stop" },
+		{
+			"<leader>gp",
+			function()
+				local prompts = {
+					"Explain",
+					"Review",
+					"Fix",
+					"Optimize",
+					"Docs",
+					"Tests",
+					"Commit",
+				}
+
+				vim.ui.select(prompts, { prompt = "Choose a prompt:" }, function(prompt)
+					if prompt then
+						vim.cmd("CopilotChat" .. prompt .. "<CR>")
+					end
+				end)
+			end,
+			mode = { "n", "v" },
+        desc = "Copilot: Prompt",
+		},
+	},
+	dependencies = {
+		{ "github/copilot.vim" },
+	},
+	build = "make tiktoken", -- Only on MacOS or Linux
+	opts = {
+		model = "claude-sonnet-4.5",
+		highlight_headers = false,
+		error_header = "> [!ERROR] Error",
+		mappings = {
+			complete = {
+				insert = "<C-p>",
+			},
+			reset = {
+				normal = "<C-r>",
+				insert = "<C-r>",
+			},
+		},
+	},
+}
--- .config/nvim/lua/plugins/editor/copilot.lua
+++ .config/nvim/lua/plugins/editor/copilot.lua
@@ -1,69 +1,12 @@
 return {
-	{
-		"github/copilot.vim",
-		-- cmd = { "Copilot" },
-		-- ft = { "python", "lua", "typescript", "markdown", "terraform" },
-		cond = function()
-			return vim.fn.executable("gh") == 1
-		end,
-		init = function()
-			vim.keymap.set("i", "<M-w>", "<Plug>(copilot-accept-word)")
-			vim.keymap.set("i", "<M-e>", "<Plug>(copilot-accept-line)")
-		end,
-	},
-	{
-		"CopilotC-Nvim/CopilotChat.nvim",
-		-- cmd = { "CopilotChat" },
-		cond = function()
-			return vim.fn.executable("gh") == 1
-		end,
-    --stylua: ignore
-		keys = {
-			{ "<leader>gg", function() require("CopilotChat").open({ window = { layout = "vertical" } }) end, mode = { "n", "v" }, desc = "Copilot: Open (Vertical)" },
-			{ "<leader>gs", function() require("CopilotChat").open({ window = { layout = "horizontal" } }) end, mode = { "n", "v" }, desc = "Copilot: Open (Horizontal)" },
-			{ "<leader>gr", ":CopilotChatReset<CR>", mode = { "n", "v" }, desc = "Copilot: Reset" },
-			{ "<leader>gx", ":CopilotChatClose<CR>", mode = { "n", "v" }, desc = "Copilot: Close" },
-			{ "<leader>gq", ":CopilotChatStop<CR>", mode = { "n", "v" }, desc = "Copilot: Stop" },
-			{
-				"<leader>gp",
-				function()
-					local prompts = {
-						"Explain",
-						"Review",
-						"Fix",
-						"Optimize",
-						"Docs",
-						"Tests",
-						"Commit",
-					}
-
-					vim.ui.select(prompts, { prompt = "Choose a prompt:" }, function(prompt)
-						if prompt then
-							vim.cmd("CopilotChat" .. prompt .. "<CR>")
-						end
-					end)
-				end,
-				mode = { "n", "v" },
-        desc = "Copilot: Prompt",
-			},
-		},
-		dependencies = {
-			{ "github/copilot.vim" },
-		},
-		build = "make tiktoken", -- Only on MacOS or Linux
-		opts = {
-			model = "claude-sonnet-4.5",
-			highlight_headers = false,
-			error_header = "> [!ERROR] Error",
-			mappings = {
-				complete = {
-					insert = "<C-p>",
-				},
-				reset = {
-					normal = "<C-r>",
-					insert = "<C-r>",
-				},
-			},
-		},
-	},
+	"github/copilot.vim",
+	-- cmd = { "Copilot" },
+	-- ft = { "python", "lua", "typescript", "markdown", "terraform" },
+	cond = function()
+		return vim.fn.executable("gh") == 1
+	end,
+	init = function()
+		vim.keymap.set("i", "<M-w>", "<Plug>(copilot-accept-word)")
+		vim.keymap.set("i", "<M-e>", "<Plug>(copilot-accept-line)")
+	end,
 }
