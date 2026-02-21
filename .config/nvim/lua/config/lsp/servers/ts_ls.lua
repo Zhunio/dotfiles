9f7fa7d3d6e4487947e973e20aecd1bf10fad9eb
Richard Zhunio <967968+Zhunio@users.noreply.github.com>
2026-02-21 13:56:36 -0500

    refactor ts_ls vue plugin path resolution

--- .config/nvim/lua/config/lsp/servers/ts_ls.lua
+++ .config/nvim/lua/config/lsp/servers/ts_ls.lua
@@ -1,9 +1,20 @@
 local on_attach = require("config.lsp.on_attach")
 
+local function get_vue_ts_plugin_location()
+	local vue_language_server_path = vim.fs.joinpath(vim.fn.stdpath("data"), "mason", "packages", "vue-language-server")
+	if not vim.uv.fs_stat(vue_language_server_path) then
+		return nil
+	end
+
+	return vim.fs.joinpath(vue_language_server_path, "node_modules", "@vue", "language-server")
+end
+
+local vue_ts_plugin_location = get_vue_ts_plugin_location()
+
 return {
 	cmd = { "typescript-language-server", "--stdio" },
 	on_attach = function(client, bufnr)
-		on_attach()
+		on_attach(client, bufnr)
 
 		vim.keymap.set("n", "gO", function()
 			vim.lsp.buf.execute_command({
@@ -12,5 +23,22 @@ return {
 			})
 		end, { desc = "LSP: Organize Imports" })
 	end,
-	filetypes = { "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx" },
+	filetypes = {
+		"javascript",
+		"javascriptreact",
+		"javascript.jsx",
+		"typescript",
+		"typescriptreact",
+		"typescript.tsx",
+		"vue",
+	},
+	init_options = {
+		plugins = vue_ts_plugin_location and {
+			{
+				name = "@vue/typescript-plugin",
+				location = vue_ts_plugin_location,
+				languages = { "vue" },
+			},
+		} or {},
+	},
 }
