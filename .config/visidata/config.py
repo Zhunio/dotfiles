3ec743452148957399c3aa22c4f58a31efb8e837
Richard Zhunio <967968+Zhunio@users.noreply.github.com>
2026-03-05 14:55:34 -0500

    visidata

---
+++ .config/visidata/config.py
@@ -0,0 +1,15 @@
+# Base grid colors
+options.color_default = 'default on #011627'
+
+# Header rows
+options.color_default_hdr = 'bold white on #011627'
+options.color_bottom_hdr = 'underline white on #011627'
+
+# Column separators (the visual "|" between columns)
+options.color_column_sep = 'white on #011627'
+
+# Restore original current row/column highlight style
+options.color_current_col = options.color_current_row
+
+# Current cell highlight
+options.color_current_cell = 'blue'
