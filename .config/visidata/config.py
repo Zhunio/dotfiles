cae47f163322c3a6c621b9e965588d8fa65a8bbd
Richard Zhunio <967968+Zhunio@users.noreply.github.com>
2026-03-06 10:47:58 -0500

    visidata: reset theme and use transparent ui backgrounds

--- .config/visidata/config.py
+++ .config/visidata/config.py
@@ -1,35 +1,15 @@
-# Explicit VisiData theme values (v3.3-compatible)
-# Hex colors were not applied by the parser, so this keeps only effective attrs.
-
-# Background/grid
+# Use terminal default fg/bg so VisiData matches your terminal background.
 options.color_default = ''
-options.color_column_sep = ''
-
-# Top row: database columns
 options.color_default_hdr = 'bold'
 options.color_bottom_hdr = 'underline'
-options.color_current_hdr = 'bold'
-
-# Sidebar: status messages based on actions
-options.color_sidebar = ''
-options.color_sidebar_title = 'bold'
-options.color_status = 'bold'
-options.color_error = 'bold'
-options.color_warning = 'bold'
-
-# Bottom row: sheet/status/rows/highlights
-options.color_top_status = 'underline'
-options.color_active_status = 'bold'
-options.color_inactive_status = ''
-options.color_highlight_status = 'bold'
-
-# Highlighted column and row
-options.color_current_row = 'reverse'
-options.color_current_col = 'bold 16 on 231'
-options.color_current_cell = 'bold 16 on 153'
-
-# Header menu: File/Edit/View/Column/Row/Data/Plot
+options.color_column_sep = ''
 options.color_menu = ''
 options.color_menu_active = 'bold'
 options.color_menu_spec = ''
 options.color_menu_help = ''
+options.color_status = 'bold'
+options.color_top_status = ''
+options.color_active_status = ''
+options.color_inactive_status = ''
+options.color_highlight_status = 'bold'
+options.color_keystrokes = 'bold'
