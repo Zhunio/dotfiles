48369630435dff2bd001bcae9a38069ef2477bb0
Richard Zhunio <967968+Zhunio@users.noreply.github.com>
2026-03-15 11:29:38 -0400

    Make LSP keymaps buffer-local

--- .config/nvim/lua/config/lsp/on_attach.lua
+++ .config/nvim/lua/config/lsp/on_attach.lua
@@ -1,4 +1,6 @@
-return function()
-	vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, { desc = "LSP: Rename symbol" })
-	vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "LSP: Hover documentation" })
+return function(_, bufnr)
+	local opts = { buffer = bufnr }
+
+	vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, vim.tbl_extend("force", opts, { desc = "LSP: Rename symbol" }))
+	vim.keymap.set("n", "K", vim.lsp.buf.hover, vim.tbl_extend("force", opts, { desc = "LSP: Hover documentation" }))
 end
