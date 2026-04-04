f52ecb95e44d216a41abd8951784a86795fcd397
Richard Zhunio <967968+Zhunio@users.noreply.github.com>
2026-04-03 23:07:56 -0400

    don't need diagnostics here

--- .config/nvim/lua/plugins/full/lsp.lua
+++ .config/nvim/lua/plugins/full/lsp.lua
@@ -26,14 +26,6 @@ return {
     event = { "BufReadPost", "BufNewFile" },
     dependencies = { "neovim/nvim-lspconfig" },
     config = function()
-      local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
-      for type, icon in pairs(signs) do
-        local hl = "DiagnosticSign" .. type
-        vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-      end
-
-      vim.diagnostic.config({ virtual_text = true })
-
       lsp.setup(on_attach)
       require("mason-lspconfig").setup({})
     end,
