249c6c8b441ea2f6886d3ba6a132f54a36b2dca6
Richard Zhunio <967968+Zhunio@users.noreply.github.com>
2026-04-05 10:12:11 -0400

    Refactor uninstall script helpers

--- uninstall.sh
+++ uninstall.sh
@@ -1,66 +1,90 @@
 #!/bin/bash
 
-BLUE='\033[0;34m'
-BOLD='\033[1m'
-RESET='\033[0m'
+DOTFILES_DIR="$HOME/dotfiles"
 
-# save current working directory
-cwd=$(pwd)
+print_run() {
+  local blue='\033[0;34m'
+  local bold='\033[1m'
+  local reset='\033[0m'
+  echo -e "${blue}==>${reset} ${bold}$1${reset}"
+}
 
-# Changing to $HOME/dotfiles directory
-cd $HOME/dotfiles
+unstow_dotfiles() {
+  local cwd
+  cwd=$(pwd)
 
-# Uninstalling dotfiles
-echo -e "${BLUE}==>${RESET} 💀 ${BOLD}Uninstalling dotfiles${RESET}"
+  if [[ ! -d "$DOTFILES_DIR" ]]; then
+    print_run "Deleting symlinks using GNU Stow (skipped)"
+    return
+  fi
 
-# Use GNU Stow to undo the symlinks
-echo -e "${BLUE}==>${RESET} 💀 ${BOLD}Deleting symlinks using GNU Stow${RESET}"
-stow -D .
+  cd "$DOTFILES_DIR"
+  print_run "Deleting symlinks using GNU Stow"
+  stow -D .
+  cd "$cwd"
+}
 
-# go back to current working directory
-cd $cwd
+remove_zshrc_dotfiles_config() {
+  local zshrc_file="$HOME/.zshrc"
 
-# Removing dotfiles sourcing from ~/.zshrc
-zshrc_file="$HOME/.zshrc"
-if [[ -f "$zshrc_file" ]]; then
-  echo -e "${BLUE}==>${RESET} 💀 ${BOLD}Removing dotfiles config from \$HOME/.zshrc${RESET}"
+  if [[ ! -f "$zshrc_file" ]]; then
+    print_run "Removing dotfiles config from \$HOME/.zshrc (skipped)"
+    return
+  fi
+
+  print_run "Removing dotfiles config from \$HOME/.zshrc"
   awk '
     $0 != "source $HOME/dotfiles/dotfiles.sh" &&
     $0 != "export DOTFILES_PROFILE=core" &&
     $0 != "export DOTFILES_PROFILE=full" { print }
   ' "$zshrc_file" >"$zshrc_file.tmp" && mv "$zshrc_file.tmp" "$zshrc_file"
-else
-  echo -e "${BLUE}==>${RESET} 💀 Removing \$HOME/.zshrc ${BOLD}(skipped)${RESET}"
-fi
+}
 
-# Removing Brew packages
-if command -v brew >/dev/null 2>&1; then
-  echo -e "${BLUE}==>${RESET} 💀 ${BOLD}Uninstalling Homebrew packages${RESET}"
+uninstall_homebrew() {
+  if ! command -v brew >/dev/null 2>&1; then
+    print_run "Uninstalling Homebrew packages (skipped)"
+    print_run "Uninstalling Homebrew (skipped)"
+    return
+  fi
+
+  print_run "Uninstalling Homebrew packages"
   brew list -1 | xargs brew uninstall --force
 
-  echo -e "${BLUE}==>${RESET} 💀 ${BOLD}Uninstalling Homebrew${RESET}"
+  print_run "Uninstalling Homebrew"
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/uninstall.sh)" -- --force
-fi
+}
 
-paths_to_delete=(
-  "$HOME/dotfiles"
-  "$HOME/.local/share/nvim"
-  "$HOME/.cache"
-  "$HOME/.copilot"
-  "$HOME/.gnupg"
-  "$HOME/.mail"
-  "$HOME/.node-gyp"
-  "$HOME/.npm"
-  "$HOME/.oh-my-zsh"
-  "$HOME/.suitecloud-sdk"
-  "$HOME/.w3m"
-)
+remove_paths() {
+  local paths_to_delete=(
+    "$HOME/dotfiles"
+    "$HOME/.local/share/nvim"
+    "$HOME/.cache"
+    "$HOME/.copilot"
+    "$HOME/.gnupg"
+    "$HOME/.mail"
+    "$HOME/.node-gyp"
+    "$HOME/.npm"
+    "$HOME/.oh-my-zsh"
+    "$HOME/.suitecloud-sdk"
+    "$HOME/.w3m"
+  )
 
-for path_to_delete in "${paths_to_delete[@]}"; do
-  if [[ -d "$path_to_delete" ]]; then
-    echo -e "${BLUE}==>${RESET} 💀 ${BOLD}Removing $path_to_delete${RESET}"
-    rm -rf "$path_to_delete"
-  else
-    echo -e "${BLUE}==>${RESET} 💀 Removing $path_to_delete ${BOLD}(skipped)${RESET}"
-  fi
-done
+  for path_to_delete in "${paths_to_delete[@]}"; do
+    if [[ -d "$path_to_delete" ]]; then
+      print_run "Removing $path_to_delete"
+      rm -rf "$path_to_delete"
+    else
+      print_run "Removing $path_to_delete (skipped)"
+    fi
+  done
+}
+
+main() {
+  print_run "Uninstalling dotfiles"
+  unstow_dotfiles
+  remove_zshrc_dotfiles_config
+  uninstall_homebrew
+  remove_paths
+}
+
+main "$@"
