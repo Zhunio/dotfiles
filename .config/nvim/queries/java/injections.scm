df8009ff093a0202c369fb8f4691601643e5cee6
Richard Zhunio <967968+Zhunio@users.noreply.github.com>
2026-04-05 01:22:31 -0400

    Configure jdtls with nvim-jdtls

---
+++ .config/nvim/queries/java/injections.scm
@@ -0,0 +1,3 @@
+; Disable upstream Java injections for now.
+; Neovim 0.12.0 + the current java/javadoc injection query path is crashing in
+; vim.treesitter.get_range() from the conceal_line decoration provider.
