d2b884599b0d95f2138cc140595a5938f8278f6c
Richard Zhunio <zhunio@Richards-Mini.localdomain>
2026-04-08 10:58:07 -0400

    nvim: make catppuccin surfaces transparent

--- .config/nvim/lua/plugins/core/catppuccin.lua
+++ .config/nvim/lua/plugins/core/catppuccin.lua
@@ -4,27 +4,18 @@ return {
 		name = "catppuccin",
 		priority = 1000,
 		config = function()
-			local background = "#011627"
-			local panel_background = "#0b253a"
 			local border = "#5f7e97"
 			local focus_border = "#122d42"
 			local selection_background = "#1d3b53"
-			local line_highlight = panel_background
 			local C = require("catppuccin.palettes").get_palette("mocha")
 
 			local custom_highlights = {
 				-- Default
-				Normal = { bg = background },
-				NormalNC = { bg = background },
-				SignColumn = { bg = background },
-				EndOfBuffer = { bg = background },
-				CursorLine = { bg = line_highlight },
-				CursorColumn = { bg = line_highlight },
 				CursorLineNr = { fg = C.text },
 				LineNr = { fg = border },
 				Visual = { bg = selection_background },
 				Search = { bg = selection_background },
-				IncSearch = { fg = background, bg = C.yellow },
+				IncSearch = { fg = C.base, bg = C.yellow },
 				Constant = { fg = C.teal },
 				String = { fg = C.green },
 				Character = { fg = C.green },
@@ -41,15 +32,12 @@ return {
 				Exception = { fg = C.mauve },
 				Structure = { fg = C.mauve },
 				Special = { fg = C.mauve },
-				NormalFloat = { bg = panel_background },
-				FloatBorder = { fg = border, bg = panel_background },
-				FloatTitle = { fg = C.text, bg = panel_background },
-				Pmenu = { bg = panel_background },
 				PmenuSel = { bg = selection_background },
-				PmenuSbar = { bg = panel_background },
 				PmenuThumb = { bg = border },
-				WinSeparator = { fg = focus_border, bg = background },
-				VertSplit = { fg = focus_border, bg = background },
+				FloatBorder = { fg = border },
+				FloatTitle = { fg = C.text },
+				WinSeparator = { fg = focus_border },
+				VertSplit = { fg = focus_border },
 				-- Treesitter
 				["@keyword"] = { fg = C.mauve },
 				["@keyword.return"] = { fg = C.mauve },
@@ -75,30 +63,11 @@ return {
 				["@lsp.type.method"] = { fg = C.blue },
 				["@lsp.type.enumMember"] = { fg = C.teal },
 				["@lsp.type.interface"] = { fg = C.yellow },
-				-- Neogit
-				NeogitDiffHeader = { bg = background },
-				NeogitDiffContext = { bg = background },
-				NeogitDiffAdd = { bg = background },
-				NeogitDiffDelete = { bg = background },
-				NeogitHunkHeader = { bg = background },
-				NeogitActiveItem = { bg = background },
 				NeogitHunkHeaderHighlight = { fg = C.surface0, bg = C.mauve, style = { "bold" } },
-				NeogitDiffContextHighlight = { bg = background },
-				NeogitDiffAddHighlight = { bg = background },
-				NeogitDiffDeleteHighlight = { bg = background },
-				-- FzfLua
-				FzfLuaNormal = { bg = background },
-				FzfLuaBorder = { bg = background },
 			}
 
 			require("catppuccin").setup({
-				transparent_background = false,
-				color_overrides = {
-					mocha = {
-						base = background,
-						mantle = panel_background,
-					},
-				},
+				transparent_background = true,
 				integrations = {
 					diffview = true,
 				},
