8c9e3a7f135451f1768ca381fe35b46425f9d6c2
Richard Zhunio <967968+Zhunio@users.noreply.github.com>
2026-04-08 23:23:21 -0400

    nvim: align catppuccin surfaces with night owl

--- .config/nvim/lua/plugins/core/catppuccin.lua
+++ .config/nvim/lua/plugins/core/catppuccin.lua
@@ -4,77 +4,30 @@ return {
 		name = "catppuccin",
 		priority = 1000,
 		config = function()
-			local line_number = "#5f7e97"
-			local selection_background = "#1d3b53"
-			local C = require("catppuccin.palettes").get_palette("mocha")
-
-			local custom_highlights = {
-				-- Default
-				CursorLine = { bg = selection_background },
-				CursorColumn = { bg = selection_background },
-				LineNr = { fg = line_number },
-				CursorLineNr = { fg = C.text },
-				Visual = { bg = selection_background },
-				Search = { bg = selection_background },
-				IncSearch = { fg = C.base, bg = C.yellow },
-				Constant = { fg = C.teal },
-				String = { fg = C.green },
-				Character = { fg = C.green },
-				Number = { fg = C.teal },
-				Boolean = { fg = C.teal },
-				Float = { fg = C.teal },
-				Function = { fg = C.blue },
-				Identifier = { fg = C.text },
-				Label = { fg = C.mauve },
-				Keyword = { fg = C.mauve },
-				Conditional = { fg = C.mauve },
-				Repeat = { fg = C.mauve },
-				Operator = { fg = C.peach },
-				Exception = { fg = C.mauve },
-				Structure = { fg = C.mauve },
-				Special = { fg = C.mauve },
-				PmenuSel = { bg = selection_background },
-				FloatTitle = { fg = C.text },
-				FzfLuaNormal = { bg = "NONE" },
-				FzfLuaBorder = { bg = "NONE" },
-				-- Treesitter
-				["@keyword"] = { fg = C.mauve },
-				["@keyword.return"] = { fg = C.mauve },
-				["@keyword.function"] = { fg = C.mauve },
-				["@keyword.conditional"] = { fg = C.mauve },
-				["@keyword.repeat"] = { fg = C.mauve },
-				["@constant"] = { fg = C.teal },
-				["@constant.builtin"] = { fg = C.teal },
-				["@string"] = { fg = C.green },
-				["@string.escape"] = { fg = C.peach },
-				["@function"] = { fg = C.blue },
-				["@function.call"] = { fg = C.blue },
-				["@function.method"] = { fg = C.blue },
-				["@function.method.call"] = { fg = C.blue },
-				["@method"] = { fg = C.blue },
-				["@method.call"] = { fg = C.blue },
-				["@property"] = { fg = C.text },
-				["@module"] = { fg = C.yellow },
-				["@variable.member"] = { fg = C.text },
-				-- LSP semantic tokens
-				["@lsp.type.keyword"] = { fg = C.mauve },
-				["@lsp.type.function"] = { fg = C.blue },
-				["@lsp.type.method"] = { fg = C.blue },
-				["@lsp.type.enumMember"] = { fg = C.teal },
-				["@lsp.type.interface"] = { fg = C.yellow },
-				NeogitHunkHeaderHighlight = { fg = C.surface0, bg = C.mauve, style = { "bold" } },
-			}
+			local nightowl_bg = "#011627"
+			local nightowl_panel = "#0b253a"
+			local nightowl_crust = "#01111d"
+			local nightowl_line_highlight = "#021320"
 
 			require("catppuccin").setup({
-				transparent_background = true,
-				integrations = {
-					diffview = true,
+				color_overrides = {
+					mocha = {
+						base = nightowl_bg,
+						mantle = nightowl_panel,
+						crust = nightowl_crust,
+					},
 				},
-				custom_highlights = custom_highlights,
+				custom_highlights = {
+					CursorLine = { bg = nightowl_line_highlight },
+					CursorColumn = { bg = nightowl_line_highlight },
+					FzfLuaBorder = { fg = nightowl_panel, bg = nightowl_panel },
+					FzfLuaNormal = { bg = nightowl_panel },
+					NeoTreeFloatBorder = { fg = nightowl_panel, bg = nightowl_panel },
+					NeoTreeFloatNormal = { bg = nightowl_panel },
+				},
 			})
 
 			vim.cmd("colorscheme catppuccin-mocha")
-			vim.cmd("set cmdheight=1")
 		end,
 	},
 }
