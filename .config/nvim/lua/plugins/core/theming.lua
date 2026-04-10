afca9f7b31309147c0f7ae3bb853c63aac3bbaf7
Richard Zhunio <967968+Zhunio@users.noreply.github.com>
2026-04-09 22:15:55 -0400

    tokyonight: styles fixes bg

--- .config/nvim/lua/plugins/core/theming.lua
+++ .config/nvim/lua/plugins/core/theming.lua
@@ -27,7 +27,9 @@ return {
 				--- function will be called with a Highlights and ColorScheme table
 				---@param highlights tokyonight.Highlights
 				---@param colors ColorScheme
-				on_highlights = function(highlights, colors) end,
+				on_highlights = function(highlights, colors)
+					highlights.Pmenu = { fg = colors.fg, bg = "NONE" }
+				end,
 			})
 
 			vim.cmd([[colorscheme tokyonight-night]])
