68a88b9d6321c2cb7e6e0179832b1f3ddad3496f
Richard Zhunio <zhunio@Richards-Mini.localdomain>
2026-04-13 10:12:01 -0400

    diagnostics: fix

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -54,15 +54,18 @@ require("lazy").setup({
 	},
 })
 
--- Diagnostics
+vim.diagnostic.config({
+	signs = {
+		text = {
+			[vim.diagnostic.severity.ERROR] = " ",
+			[vim.diagnostic.severity.WARN] = " ",
+			[vim.diagnostic.severity.HINT] = " ",
+			[vim.diagnostic.severity.INFO] = " ",
+		},
+	},
+	virtual_text = true,
+})
 
-local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
-for type, icon in pairs(signs) do
-	local hl = "DiagnosticSign" .. type
-	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-end
-
-vim.diagnostic.config({ virtual_text = true })
 -- Keymaps
 vim.keymap.set("i", "jj", "<Esc>")
 vim.keymap.set("i", "jk", "<Esc>")
