cf849bbe64c4474c4a5b7431a688114ef3ac13d7
Richard Zhunio <967968+Zhunio@users.noreply.github.com>
2026-04-25 17:08:39 -0400

    indent-blankline: simplify it

--- .config/nvim/lua/plugins/core/auto-pairs.lua
+++ .config/nvim/lua/plugins/core/auto-pairs.lua
@@ -13,47 +13,11 @@ return {
 				require("lazy").load({ plugins = { "indent-blankline.nvim" } })
 			end, { desc = "Load indent-blankline.nvim" })
 		end,
-		opts = function()
-			local highlight = {
-				"RainbowRed",
-				"RainbowOrange",
-				"RainbowYellow",
-				"RainbowGreen",
-				"RainbowBlue",
-				"RainbowViolet",
-			}
-
-			local colors = require("tokyonight.colors").setup({ style = "nightowl" })
-			local highlight_colors = {
-				RainbowRed = colors.red,
-				RainbowOrange = colors.orange,
-				RainbowYellow = colors.yellow,
-				RainbowGreen = colors.green,
-				RainbowBlue = colors.blue,
-				RainbowViolet = colors.purple,
-			}
-
-			local hooks = require("ibl.hooks")
-			hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-				for _, highlight_name in pairs(highlight) do
-					local highlight_color = highlight_colors[highlight_name]
-					vim.api.nvim_set_hl(0, highlight_name, { fg = highlight_color })
-				end
-			end)
-
-			return {
-				indent = {
-					char = "▏",
-					highlight = highlight,
-				},
-				scope = {
-					char = "▎",
-					highlight = highlight,
-				},
-			}
-		end,
 		config = function(_, opts)
-			require("ibl").setup(opts)
+			require("ibl").setup({
+				indent = { char = "▏" },
+				scope = { char = "▎" },
+			})
 		end,
 	},
 }
