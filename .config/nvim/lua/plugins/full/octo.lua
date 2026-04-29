2b67213882fae86fc7e07692ddb0ca8b276947e4
Richard Zhunio <967968+Zhunio@users.noreply.github.com>
2026-04-29 12:24:03 -0400

    octo: fix diffview

--- .config/nvim/lua/plugins/full/octo.lua
+++ .config/nvim/lua/plugins/full/octo.lua
@@ -12,6 +12,12 @@ return {
 				picker = "fzf-lua",
 				use_local_fs = true,
 			})
+			local Layout = require("octo.reviews.layout").Layout
+			local init_layout = Layout.init_layout
+			Layout.init_layout = function(self)
+				init_layout(self)
+				vim.api.nvim_set_hl(require("octo.constants").OCTO_REVIEW_RIGHT_HIGHLIGHT_NS, "DiffText", { bg = "#004000" })
+			end
 		end,
 	},
 }
