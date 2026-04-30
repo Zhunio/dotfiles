2eb1f126744206d417cb4934d335ff27af43c767
Richard Zhunio <967968+Zhunio@users.noreply.github.com>
2026-04-30 11:25:27 -0400

    image: remove 3rd/image references

--- .config/nvim/lua/plugins/core/neo-tree.lua
+++ .config/nvim/lua/plugins/core/neo-tree.lua
@@ -7,18 +7,12 @@ return {
 			{ "<leader>ef", ":Neotree reveal=true position=float toggle=true<CR>", desc = "Neotree: Toggle Sidenav" },
 			{ "<leader>ee", ":Neotree reveal=true position=left toggle=true<CR>", desc = "Neotree: Open" },
 		},
-		-- dependencies = { { "3rd/image.nvim", lazy = true } },
 		opts = {
 			filesystem = {
 				filtered_items = {
 					visible = true,
 				},
 			},
-			-- window = {
-			-- 	mappings = {
-			-- 		["P"] = { "toggle_preview", config = { use_image_nvim = true } }, -- Add this mapping so that pressing 'P' toggles the preview using image.nvim
-			-- 	},
-			-- },
 		},
 	},
 }
