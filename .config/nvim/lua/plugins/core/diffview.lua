7d67fe53d8256f7538674b09aeada78b6d325e32
Richard Zhunio <967968+Zhunio@users.noreply.github.com>
2026-04-29 22:41:07 -0400

    diffview: improve

--- .config/nvim/lua/plugins/core/diffview.lua
+++ .config/nvim/lua/plugins/core/diffview.lua
@@ -2,24 +2,33 @@ return {
 	{
 		"sindrets/diffview.nvim",
 		cmd = { "DiffviewOpen", "DiffviewFileHistory" },
-    --stylua: ignore
-		opts = {
-			enhanced_diff_hl = true,
-		},
-		init = function()
+		config = function()
 			-- Fix dash lines in diff view (https://github.com/sindrets/diffview.nvim/issues/35)
 			-- vim.opt.fillchars = vim.opt.fillchars + "diff:╱"
 			vim.opt.fillchars = vim.opt.fillchars + "diff: "
 
-			local function set_diffview_keymaps()
-				local keymapOptions = { noremap = true, silent = true, buffer = true }
+			local actions = require("diffview.actions")
 
-				vim.keymap.set("n", "q", ":tabclose<CR>", keymapOptions)
-			end
-
-			vim.api.nvim_create_autocmd("FileType", {
-				pattern = "Diffview*",
-				callback = set_diffview_keymaps,
+			require("diffview").setup({
+				enhanced_diff_hl = true,
+        --stylua: ignore
+				keymaps = {
+					view = {
+						{ "n", "-", actions.toggle_stage_entry, { desc = "Stage / unstage the selected entry" } },
+						{ "n", "gd", function() actions.goto_file_edit() vim.lsp.buf.definition() end },
+					},
+					file_panel = {
+						{ "n", "<cr>", actions.focus_entry, { desc = "Focus the selected entry" } },
+						{ "n", "s", actions.toggle_stage_entry, { desc = "Stage / unstage the selected entry" } },
+						{ "n", "[c", actions.view_windo(function(_, sym) if sym == "b" then vim.cmd("norm! [c") end end), },
+						{ "n", "]c", actions.view_windo(function(_, sym) if sym == "b" then vim.cmd("norm! ]c") end end), },
+					},
+					file_history_panel = {
+						{ "n", "[n", actions.select_prev_commit },
+						{ "n", "]n", actions.select_next_commit },
+						{ "n", "<cr>", actions.focus_entry, { desc = "Focus the selected entry" } },
+					},
+				},
 			})
 		end,
 	},
