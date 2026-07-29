8220eaef514cfbabec42b23a60a02dc213f8ea0c
Richard Zhunio <967968+Zhunio@users.noreply.github.com>
2026-07-28 20:57:03 -0400

    fix(nvim): restore session autocmd nesting

--- .config/nvim/lua/plugins/persistence.lua
+++ .config/nvim/lua/plugins/persistence.lua
@@ -10,16 +10,17 @@ return {
 		},
 		config = function()
 			vim.opt.sessionoptions = {
-				"folds", -- restore fold state and fold-related local options
-				"help", -- restore help windows
-				"resize", -- restore editor size using 'lines' and 'columns'
-				"tabpages", -- restore all tab pages
-				"winpos", -- restore the outer editor window position
-				"winsize", -- restore split window sizes
+				"blank",
+				"buffers",
+				"curdir",
+				"folds",
+				"help",
+				"tabpages",
+				"terminal",
+				"winsize",
 			}
 			require("persistence").setup({
 				need = 0, -- Set to 0 to always save
-				branch = true, -- use git branch to save session
 			})
 
 			vim.api.nvim_create_autocmd("VimEnter", {
