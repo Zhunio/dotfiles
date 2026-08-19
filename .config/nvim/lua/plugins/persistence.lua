a732b936e25a19be5deaa42834b65adf44411334
Richard Zhunio <967968+Zhunio@users.noreply.github.com>
2026-08-19 10:32:15 -0500

    delete current seccion

--- .config/nvim/lua/plugins/persistence.lua
+++ .config/nvim/lua/plugins/persistence.lua
@@ -1,3 +1,16 @@
+local function delete_current_session()
+	local persistence = require("persistence")
+	local session = persistence.current()
+
+	persistence.stop()
+
+	if vim.fn.delete(session) == 0 then
+		vim.notify("Deleted session: " .. session)
+	else
+		vim.notify("No current session to delete", vim.log.levels.WARN)
+	end
+end
+
 return {
 	{
 		"folke/persistence.nvim",
@@ -6,7 +19,7 @@ return {
 		keys = {
 			{ "<leader>zl", mode = { "n" }, function() require("persistence").load() end },
 			{ "<leader>zf", mode = { "n" }, function() require("persistence").select() end },
-			{ "<leader>zx", mode = { "n" }, function() require("persistence").stop() end }
+			{ "<leader>zx", mode = { "n" }, delete_current_session },
 		},
 		config = function()
 			vim.opt.sessionoptions = {
